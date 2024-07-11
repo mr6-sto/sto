1a70391bb85c08960ef2f14b193b8134967190f0
mew <666>
2024-07-11 15:21:36 +0800

    day01

---
+++ src/test/java/com/macro/mall/tiny/malltiny01/MallTiny01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macro.mall.tiny.malltiny01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallTiny01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
